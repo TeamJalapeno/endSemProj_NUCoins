3af88eb375a8c748ede38fca7d227b080d73a4dc
taimoorbinkhalid <taimoorkhalid95@gmail.com>
2016-05-07 16:15:51 +0500

    Login/Authentication - Version 2.0

--- app/content/login/login_js/login_app.js
+++ app/content/login/login_js/login_app.js
@@ -3,3 +3,7 @@
 /* App Module */
 
 var LoginApp = angular.module("NCLoginApp", ['NCAnimations', 'NCLoginControllers']);
+
+$('.message a').click(function(){
+   $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
+});
--- app/content/login/login_js/login_controllers.js
+++ app/content/login/login_js/login_controllers.js
@@ -7,31 +7,33 @@ var NCLoginController = angular.module('NCLoginControllers', ['firebase']);
 NCLoginController.controller('LoginCtrl', ['$scope', '$location', '$firebaseAuth', function($scope, $location, $firebaseAuth) {
   var firebaseObj = new Firebase("https://nucoins.firebaseio.com/");
   var loginObj = $firebaseAuth(firebaseObj);
+
   var emailAuth = new Firebase("https://nucoins.firebaseio.com/users/email");
+  var usersAccount = new Firebase("https://nucoins.firebaseio.com/usersData");
 
-  $scope.loginBool = true;
-  $scope.signupBool = false;
-
-  $scope.SignUp = function(e) {
-    e.preventDefault();
-
-    console.log('hi');
-
-    $scope.loginBool = false;
-    $scope.signupBool = true;
+  var userAcc = false;
+  var myStyle2 = {
+    'display':'none'
   }
+  $scope.myStyle2 = myStyle2;
 
-  $scope.Login = function(e) {
-    e.preventDefault();
-
-    console.log('bye');
-
-    $scope.loginBool = true;
-    $scope.signupBool = false;
+  var myStyle3 = {
+    'display':'none'
   }
+  $scope.myStyle3 = myStyle3;
+  $scope.authEmailError = false;
 
-
+  var absUrl = "";
   $scope.user = {};
+
+  function escapeEmailAddress(email) {
+    if (!email) return false
+
+    email = email.toLowerCase();
+    email = email.substring(0, email.indexOf("@"));
+    return email;
+  }
+
   $scope.SignIn = function(e) {
     e.preventDefault();
     var username = $scope.user.email;
@@ -43,16 +45,19 @@ NCLoginController.controller('LoginCtrl', ['$scope', '$location', '$firebaseAuth
     .then(function(user) {
       //Success callback
       console.log('Authentication successful');
-      //$location.path('../home/homepage');
+      absUrl = $location.absUrl();
+      absUrl = absUrl.substring(0, absUrl.indexOf("/login/login.html"));
+
+      absUrl = absUrl + "/home/home.html";
+      window.location.replace(absUrl);
     }, function(error) {
       //Failure callback
       console.log('Authentication failure');
     });
   }
 
-  $scope.Register = function(e) {
+  $scope.AuthEmail = function(e) {
     e.preventDefault();
-
     var email = $scope.user.email;
     var auth = false;
 
@@ -63,38 +68,90 @@ NCLoginController.controller('LoginCtrl', ['$scope', '$location', '$firebaseAuth
           break;
         }
       }
-      console.log(snapshot.val());
-      console.log(auth);
+
+      if (auth) {
+        $scope.authEmailError = false;
+        usersAccount.on("value", function(snapshot) {
+
+          userAcc = snapshot.hasChild(escapeEmailAddress(email));
+          console.log("Account Exists? " + userAcc);
+
+          if (userAcc) {
+            var myStyle = {
+              'display':'none'
+            }
+
+            $scope.myStyle = myStyle;
+            myStyle2 = {
+              'display':'block'
+            }
+            $scope.myStyle2 = myStyle2;
+          }
+          else if(!userAcc) {
+            var myStyle = {
+              'display':'none'
+            }
+            $scope.myStyle = myStyle;
+
+            myStyle3 = {
+              'display':'block'
+            }
+            $scope.myStyle3 = myStyle3;
+          }
+
+        }, function (errorObject) {
+          console.log("The read failed: " + errorObject.code);
+        });
+      }
+
+      else if (!auth) {
+        $scope.authEmailError = true;
+      }
+
     }, function (errorObject) {
       console.log("The read failed: " + errorObject.code);
     });
+  }
 
-    var password = $scope.user.password;
+  $scope.RegisterUser = function(e) {
+    e.preventDefault();
+
     var firstName = $scope.user.firstName;
     var lastName = $scope.user.lastName;
-    //console.log(auth);
-    if (email && password && auth) {
-      var usersRef = firebaseObj.child("userData");
-      usersRef.push({'First Name': firstName, 'Last Name': lastName});
+    var gender = $scope.user.gender;
+    var email = $scope.user.email;
+    var password = $scope.user.password;
+
+    if (email && password) {
+      var usersRef = usersAccount.child(escapeEmailAddress(email))
+      usersRef.set({
+        'First Name': firstName,
+        'Last Name': lastName,
+        'Gender': gender,
+        'Email': email
+      });
 
       loginObj.$createUser(email, password)
-        .then(function() {
-          // do things if success
-          //$location.path('/loginpage');
-          console.log('User creation success');
-          console.log(firstName);
-          console.log(lastName);
-        }, function(error) {
-          // do things if failure
-          console.log(error);
-          $scope.regError = true;
-          $scope.regErrorMessage = error.message;
-        });
-    }
-    else if (!auth) {
-      $scope.regError = true;
-      $scope.regErrorMessage = "Please use your NUST Email ID to create account on NUCoins.";
-      console.log($scope.regErrorMessage);
+      .then(function() {
+        console.log('User creation success');
+        console.log(firstName);
+        console.log(lastName);
+
+        var myStyle3 = {
+          'display':'none'
+        }
+
+        $scope.myStyle3 = myStyle3;
+        myStyle2 = {
+          'display':'block'
+        }
+        $scope.myStyle2 = myStyle2;
+      }, function(error) {
+
+        console.log(error);
+        $scope.regError = true;
+        $scope.regErrorMessage = error.message;
+      });
     }
   };
 }]);
