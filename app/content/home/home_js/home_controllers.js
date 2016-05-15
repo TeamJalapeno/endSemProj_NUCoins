cf611c02a9a5b05aeb6656d66fcecf497b52b6e1
efatima <14bscsefatima@seecs.edu.pk>
2016-05-15 20:36:05 +0500

    Admin to Student NUCoins transfer has been implemented

--- app/content/home/home_js/home_controllers.js
+++ app/content/home/home_js/home_controllers.js
@@ -51,6 +51,9 @@ NCMainControllers.controller('EventListCtrl', function($scope, $firebaseArray) {
 });
 
 
+
+
+
 NCMainControllers.controller('AddAmountCtrl', function($scope, $firebaseObject, $cookies) {
   console.log('AddAmountCtrl');
   var email = $cookies.get('sessionCookie');
@@ -60,28 +63,83 @@ NCMainControllers.controller('AddAmountCtrl', function($scope, $firebaseObject, 
   console.log(email);
   $scope.userEmail = $cookies.get('sessionCookie');
 
-  $scope.amount = function(){
-    console.log('amount function');
-    var ref = new Firebase("https://nucoins.firebaseio.com/usersData/"+email+"/Balance");
-    var usertype = new Firebase("https://nucoins.firebaseio.com/usersData/"+email);
+  $scope.amount = function(e){
+   console.log('amount function');
+    var id = $scope.user.studentid;   //will have user 2's email id
+    var amount = $scope.user.amount;  //the amount to be transferred
+    console.log(id);
+    console.log(amount);
+    id = id.substring(0, id.indexOf("@"));  //extracting user2's id from email
+    console.log(id);
+    id = id.toLowerCase();
+    id = id.toString();
+    console.log("User2's id:" +id);
+    console.log("Processing Transfer Request..");
+
+
+    var ref = new Firebase("https://nucoins.firebaseio.com/usersData/"+email+"/AccessLevel");
     var obj = new $firebaseObject(ref);
-    $scope.userType = $firebaseObject(usertype);
-    //hide addAmount if user is not an admin
-    // jq('.loginerrormessage').hide();
-    obj.$loaded().then(function() {
-      var sum =0;
-      sum = (parseInt(obj.$value));
-      console.log("Current Balance: "+sum);
-      sum = sum + 500;
-      console.log("After adding 500: "+sum);
-      obj.$value = sum;
-      obj.$save();
-      console.log('New current Balance:'+ obj.$value);
+
+    var ref2 = new Firebase("https://nucoins.firebaseio.com/usersData/"+id+"/AccessLevel");
+    var obj2 = new $firebaseObject(ref2);
+
+    var ref3 = new Firebase("https://nucoins.firebaseio.com/usersData/"+email+"/Balance");   // accesing user 1's balance from the databse
+    var obj3 = new $firebaseObject(ref3);
+
+    var ref4 = new Firebase("https://nucoins.firebaseio.com/usersData/"+id+"/Balance");   // accesing user 2's balance from the databse
+    var obj4 = new $firebaseObject(ref4);
+
+    obj.$loaded(),obj2.$loaded(),obj3.$loaded().then(function(){
+        var first = ""
+        var second = ""
+        var existingBal = 0 ;
+        var studentBal =0;
+      first = obj.$value;
+      second= obj2.$value;
+      existingBal = parseInt(obj3.$value);    //user 1's existing balance in the database
+      studentBal = parseInt(obj4.$value);    //user 2's existing balance in the database
+
+      console.log("user1's access level is:" +first);
+      console.log("user2's access level is:" +second);
+      console.log("User1's existingBal: " + existingBal);
+      console.log("User2's existingBal: " + studentBal);
+
+
+      if(first == "admin" && second =="student"){  // if user 1 is admin and user 2 is a student
+        console.log("Admin to Student Transfer Request.....")
+            if(existingBal >= amount)  {//checking if the amount user wants to transfer is available in his account
+            console.log("Valid Trasfer request");
+
+            console.log("Admin's Current Balance: "+ existingBal);
+            existingBal = existingBal - amount;
+            obj3.$value = existingBal;
+            obj3.$save();
+            console.log("After subtracting amount, new bal: "+ existingBal);
+
+            console.log("Student's Current Balance: "+ studentBal);
+            studentBal = studentBal + amount;
+            obj4.$value = studentBal;
+            obj4.$save();
+            console.log("After adding amount, new bal: "+ studentBal);
+
+             console.log("Admin to Student Transfer Completed");
+           }
+           else{
+              console.log("Your account balance is insufficient");
+           }
+      }
+      else{
+          console.log("Invalid Transfer Request");
+      }
+
     });
 
   }
 
 });
+
+
+
 
 NCMainControllers.controller('EventDetailCtrl', ['$scope', '$stateParams', '$http',
   function($scope, $stateParams, $http) {
